5f4c2218bb59e5b4bfe0f4f0c7d3d47d509df8d2
hexudong <hexudong>
2020-11-03 18:23:39 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    devServer: {
+        /* 本地ip地址 */
+        host: "192.168.1.180",
+        port: "9010",
+        hot: true,
+        /* 自动打开浏览器 */
+        open: true
+    }
+}
